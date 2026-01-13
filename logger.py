85e2b3ad696d45fd29737070cfa8dadc2218fb68
nikmoon007 <152786118+nikmoon007@users.noreply.github.com>
2026-01-13 23:08:46 +0500

    Enhance Logger with HTTP logging and exit handling
    1. Добавил запись http логов
    2. добавил запись исключений при вылете программы

--- logger.py
+++ logger.py
@@ -2,13 +2,16 @@ import logging
 import json
 import inspect
 from datetime import datetime
+import atexit
+import sys
+import traceback
 
 
 class LoggerSaveJSL:
     '''
     Вызов записи объекта класса -
         logger.info("Просто лог", extra={'params_name': 'ManualLog', 'params_value': 100})
-        При вызове логгера  в экстре для save_python_logs обязательных 2 параметра:
+        При вызове логгера в экстре для save_python_logs обязательных 2 параметра:
         params_name и params_value
     '''
 
@@ -25,7 +28,8 @@ class LoggerSaveJSL:
         'end_funcs': 'окончание работы функции',
         'start_class': 'Сформирован объект класса',
         'str_obj': 'определен объект',
-        'chan_mod': 'Изменен объект'
+        'chan_mod': 'Изменен объект',
+        'start_https': 'отправка сетевого запроса'
     }
 
     def __init__(self, Log_LVL):
@@ -79,6 +83,31 @@ class LoggerSaveJSL:
         }
         return json.dumps(log_data, ensure_ascii=False) + '\n'
 
+    def __format_http_json(self, record):
+        """Отдельная функция для формирования лога для HTTP запросов"""
+        log_data = {
+            'timestamp': datetime.fromtimestamp(record.created).isoformat(),
+            'level': record.levelname,
+            'message': record.getMessage(),
+            'status_code': getattr(record, 'status_code', 'unknown'),
+            'headers' : getattr(record, 'headers', 'unknown'),
+            'url' : getattr(record, 'url', 'unknown'),
+            'request' : getattr(record, 'request', 'unknown'),
+            'cookies' : self.__process_value(getattr(record, 'cookies', 'unknown')),
+            'elapsed' : getattr(record, 'elapsed', 'unknown'),
+            'result' : self.__process_value(getattr(record, 'result', 'unknown')),
+        }
+        return json.dumps(log_data, ensure_ascii=False) + '\n'
+
+    def __format_http_console(self, record):
+        """Форматирование записи для вывода в консоль"""
+        dt = datetime.fromtimestamp(record.created).strftime('%Y-%m-%d %H:%M:%S')
+        s_code = getattr(record, 'status_code', 'default')
+        eled = getattr(record, 'elapsed', 'unknown')
+        reqst = getattr(record, 'request', 'unknown')
+        url = getattr(record, 'url', 'unknown')
+        return f"{dt} - {record.levelname} - {record.getMessage()} \n{s_code} - {eled} - {reqst} - {url}"
+
     def __format_python_console(self, record):
         """Форматирование записи для вывода в консоль"""
         dt = datetime.fromtimestamp(record.created).strftime('%Y-%m-%d %H:%M:%S')
@@ -91,7 +120,7 @@ class LoggerSaveJSL:
         lvl_consol_handler = self.__chec_lvl(lvl_consol_handler)
 
         # ____Создаю логгер и объявляю уровень записи____
-        logger = logging.getLogger('my_logger')
+        logger = logging.getLogger('my_logger_python')
         logger.setLevel(self.dict_log_lvl[self.Log_LVL])
 
         if not logger.handlers:
@@ -118,3 +147,119 @@ class LoggerSaveJSL:
             logger.addHandler(consol_handler)
 
         return logger
+
+    def save_http_logs(self, file_name, lvl_file_handler='DEBUG', lvl_consol_handler='WARNING'):
+        # ____Проверяю уровни на корректность____
+        lvl_file_handler = self.__chec_lvl(lvl_file_handler)
+        lvl_consol_handler = self.__chec_lvl(lvl_consol_handler)
+
+        # ____Создаю логгер и объявляю уровень записи____
+        logger = logging.getLogger('my_logger_http')
+        logger.setLevel(self.dict_log_lvl[self.Log_LVL])
+
+        if not logger.handlers:
+            # ____Создаю хендлеры для консоли и для файла____
+            file_handler = logging.FileHandler(filename=file_name, mode='a', encoding='utf-8')
+            consol_handler = logging.StreamHandler()
+
+            # ____Объявляю уровни записей хендлерров____
+            file_handler.setLevel(self.dict_log_lvl[lvl_file_handler])
+            consol_handler.setLevel(self.dict_log_lvl[lvl_consol_handler])
+
+            # ____Прикрепляю к хендлеррам форматтеры____
+            # Мы используем метод подмены функции format у стандартного Formatter
+            f_formatter = logging.Formatter()
+            f_formatter.format = self.__format_http_json
+            file_handler.setFormatter(f_formatter)
+
+            c_formatter = logging.Formatter()
+            c_formatter.format = self.__format_http_console
+            consol_handler.setFormatter(c_formatter)
+
+            # ____Прикрепляю Хендлерры к логгеру____
+            logger.addHandler(file_handler)
+            logger.addHandler(consol_handler)
+
+        return logger
+
+    def setup_exit_logging(self, logger, file_name=None, handle_errors=True):
+        """
+        Настраивает логирование состояния при завершении программы с помощью atexit.
+        :param logger: Логгер, в который записывать (python или http).
+        :param file_name: Опционально, отдельный файл для exit-логов (если не указан, использует существующий хендлер).
+        :param handle_errors: Если True, также устанавливает sys.excepthook для логирования непойманных ошибок.
+        """
+        if file_name:
+            # Добавляем отдельный хендлер для exit-логов, если указан
+            exit_handler = logging.FileHandler(filename=file_name, mode='a', encoding='utf-8')
+            # Используем подходящий форматтер в зависимости от типа логгера
+            if logger.name == 'my_logger_python':
+                f_formatter = logging.Formatter()
+                f_formatter.format = self.__format_python_json
+                exit_handler.setFormatter(f_formatter)
+            else:  # Для HTTP
+                f_formatter = logging.Formatter()
+                f_formatter.format = self.__format_http_json
+                exit_handler.setFormatter(f_formatter)
+            logger.addHandler(exit_handler)
+
+        if handle_errors:
+            # Сохраняем оригинальный excepthook
+            original_excepthook = sys.excepthook
+
+            def my_excepthook(exc_type, exc_value, exc_traceback):
+                # Извлекаем детали ошибки
+                error_details = {
+                    'type': exc_type.__name__,
+                    'message': str(exc_value),
+                    'full_traceback': ''.join(traceback.format_tb(exc_traceback)),
+                    'last_line': traceback.format_tb(exc_traceback)[-1] if exc_traceback else 'N/A'
+                    # Последняя строчка стека
+                }
+
+                # Логируем с extra для python-логгера
+                if logger.name == 'my_logger_python':
+                    logger.error("Непойманная ошибка!", extra={
+                        'params_name': 'ErrorDetails',
+                        'params_value': error_details
+                    })
+                else:  # Для HTTP просто сообщение
+                    logger.error(f"Непойманная ошибка! Детали: {error_details}")
+
+                # Вызываем оригинальный excepthook для консольного вывода
+                original_excepthook(exc_type, exc_value, exc_traceback)
+
+            # Устанавливаем хук
+            sys.excepthook = my_excepthook
+
+        def log_on_exit():
+            global_vars = globals().copy()  # Копируем глобальные переменные
+            # Убираем служебные, чтобы не засорять лог (опционально)
+            global_vars.pop('__builtins__', None)
+            global_vars.pop('__name__', None)
+            global_vars.pop('__doc__', None)
+            global_vars.pop('__package__', None)
+            global_vars.pop('__loader__', None)
+            global_vars.pop('__spec__', None)
+
+            # Логируем с extra для совместимости с форматом
+            if logger.name == 'my_logger_python':
+                logger.info("Программа завершается.", extra={
+                    'params_name': 'ExitState',
+                    'params_value': global_vars
+                })
+            else:  # Для HTTP просто сообщение (без extra, т.к. формат отличается)
+                logger.info(f"Программа завершается. Состояние: {global_vars}")
+
+        # Регистрируем только раз
+        atexit.register(log_on_exit)
+
+"""
+Примеры подключения логгера:
+logg = LoggerSaveJSL('DEBUG')
+logger = logg.save_python_logs('text_test.logs') # здесь может быть подключен метод для записи http лога - save_http_logs
+logg.setup_exit_logging(logger) # вызывается 1 раз, ловит все ошибки и записывает их в файл
+# Когда нужно вызвать лог для записи данных, просто пиши в каждую функцию/класс итд, чем больше таких вызовов, тем больше запишется
+logger.info("Просто лог", extra={'params_name': 'ManualLog', 'params_value': 100})
+
+"""
